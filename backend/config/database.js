1c0fbd9278bb9fc931547fc4f9189df6e00f39cc
Priyansh B <priyansh292005@gmail.com>
2025-07-10 15:21:36 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://priyansh292005:Rishav12@cluster0.hjdexe1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
